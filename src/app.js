dcf274ad959047733375c0aba457db72792442f7
Jarraxus <buckman.joshua@gmail.com>
2022-01-10 19:39:00 +0000

    final upload

--- src/app.js
+++ src/app.js
@@ -5,57 +5,32 @@ import "./style.css";
 
 window.onload = () => {
   document.querySelector("#btn").addEventListener("click", () => {
-    document.querySelector("#the-excuse").innerHTML = generateExcuse();
+    document.querySelector("#domain-name").innerHTML = generateExcuse();
   });
 };
 
 let generateExcuse = () => {
-  let pronoun = ["A", "The", "My"];
-  let subject = [
-    "dog",
-    "grandfather",
-    "girlfriend",
-    "boss",
-    "teacher",
-    "best friend"
+  let pronoun = ["that", "the", "my", "our"];
+  let adjective = [
+    "lustful",
+    "gluttonous",
+    "greedy",
+    "slothful",
+    "wrathful",
+    "envious",
+    "prideful"
   ];
-  let action = [
-    "ate my",
-    "lost my",
-    "yelled at my",
-    "stole my",
-    "bit my",
-    "blew up my"
-  ];
-  let possession = [
-    "homework",
-    "medication",
-    "will to live",
-    "paycheck",
-    "edible shoes"
-  ];
-  let where = [
-    "on the street",
-    "in my driveway",
-    "in my bed",
-    "at the park",
-    "in the toilet"
-  ];
+  let noun = ["bear", "snake", "rabbit", "wolf", "eagle", "badger", "panther"];
+  let domain = [".com", ".biz", ".net", ".us", ".eu"];
 
   let pronounIndex = Math.floor(Math.random() * pronoun.length);
-  let subjectIndex = Math.floor(Math.random() * subject.length);
-  let actionIndex = Math.floor(Math.random() * action.length);
-  let possessionIndex = Math.floor(Math.random() * possession.length);
-  let whereIndex = Math.floor(Math.random() * where.length);
+  let adjectiveIndex = Math.floor(Math.random() * adjective.length);
+  let nounIndex = Math.floor(Math.random() * noun.length);
+  let domainIndex = Math.floor(Math.random() * domain.length);
   return (
     pronoun[pronounIndex] +
-    " " +
-    subject[subjectIndex] +
-    " " +
-    action[actionIndex] +
-    " " +
-    possession[possessionIndex] +
-    " " +
-    where[whereIndex]
+    adjective[adjectiveIndex] +
+    noun[nounIndex] +
+    domain[domainIndex]
   );
 };
